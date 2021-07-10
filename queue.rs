de79f330fcf496612b48753f06eb2bcb742abe4a
NotaCSstudent <mahmudbabu94@gmail.com>
2021-07-10 10:42:20 -0400

    added rust

---
+++ queue.rs
@@ -0,0 +1,17 @@
+use std::collections::VecDeque;
+
+pub fn queue()
+{
+    let mut _list = VecDeque::new();
+    _list.push_back(3);
+    _list.push_back(4);
+    _list.push_back(5);
+
+
+
+}
+
+fn main()
+{
+
+}
---
+++ stack.rs
@@ -0,0 +1,10 @@
+use std::collections::VecDeque;
+
+pub fn stack()
+{
+    let mut _stack = VecDeque::new();
+}
+fn main()
+{
+    
+}
